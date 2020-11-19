16fb59fd1fd5d5d722528dfb450796cd85cf7e07
Daniel Angulo <dangulos@unal.edu.co>
2020-11-19 00:39:23 -0500

    b

--- rgba_to_grey.cu
+++ rgba_to_grey.cu
@@ -15,7 +15,7 @@ void rgba_to_grey_kernel(const uchar4* const rgbaImage,
 
   int id = (blockDim.x * blockIdx.x) + threadIdx.x;
 
-  initIteration = (852.0*480.0/totalThreads) * id;
+  initIteration = (852*480/totalThreads) * id;
 
   if (id == totalThreads - 1)
     endIteration = 852*480;
@@ -31,7 +31,7 @@ void rgba_to_grey_kernel(const uchar4* const rgbaImage,
     int x = j * (numCols/852.0);
     int y = i * (numRows/480.0);
   
-    int indexAux = (x + y * numCols);
+    int indexAux = (x + y * numCols)*3;
     uchar4 px = rgbaImage[indexAux]; // thread pixel to process
     greyImage[index + 2] = px.x; 
     greyImage[index + 1] = px.y; 
