a9d9beb0f0d75f37be882ff4ea3419b5567b60ca
Daniel Angulo <dangulos@unal.edu.co>
2020-11-19 13:27:10 -0500

    small change

--- reduction.cu
+++ reduction.cu
@@ -10,34 +10,18 @@ void reduction_kernel(const uchar4* const rgbaImage,
                        unsigned char* const outputImage,
                        int numRows, int numCols, int totalThreads)
 {
+  int aux = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = aux % 852;
+  int i = (aux - j) / 852;
+  int index = (j + i * 852) * 3;
+  int x = j * (numCols/852.0);
+  int y = i * (numRows/480.0);
 
-  int initIteration, endIteration;
-
-  int id = (blockDim.x * blockIdx.x) + threadIdx.x;
-
-  initIteration = (852*480/totalThreads) * id;
-
-  if (id == totalThreads - 1)
-    endIteration = 852*480;
-  else
-    endIteration = initIteration + ((852*480 / totalThreads));
-
-  int index = 0;
-
-  for (int aux = initIteration; aux < endIteration; aux++){
-    int j = aux % 852;
-    int i = (aux - j) / 852;
-    index = aux * 3;
-    int x = j * (numCols/852.0);
-    int y = i * (numRows/480.0);
-  
-    int indexAux = (x + y * numCols);
-    uchar4 px = rgbaImage[indexAux]; // thread pixel to process
-    outputImage[index + 2] = px.x; 
-    outputImage[index + 1] = px.y; 
-    outputImage[index] = px.z;
-    
-  }
+  int indexAux = (x + y * numCols);
+  uchar4 px = rgbaImage[indexAux]; // thread pixel to process
+  greyImage[index + 2] = px.x; 
+  greyImage[index + 1] = px.y; 
+  greyImage[index] = px.z;
 }
 
 void reduction(uchar4 * const d_originalImage,
@@ -46,9 +30,14 @@ void reduction(uchar4 * const d_originalImage,
 
   
   int totalThreads = aBlockSize * aGridSize;
+
+  // const dim3 blockSize(aBlockSize, 1, 1);
+  // const dim3 gridSize(aGridSize, 1, 1);
+  long long int total_px = 852*480;  // total pixels
+  long int grids_n = ceil(total_px / aBlockSize); // grids numer
   const dim3 blockSize(aBlockSize, 1, 1);
-  const dim3 gridSize(aGridSize, 1, 1);
-  reduction_kernel<<<gridSize, blockSize>>>(d_originalImage, d_resizeImage, numRows, numCols, totalThreads);
+  const dim3 gridSize(grids_n, 1, 1);
+  rgba_to_grey_kernel<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols, totalThreads);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 }
